7ae95d411d5ea642b368c36c7c5cd0b80891a540
Nomel333 <wns5861@naver.com>
2022-05-16 10:59:55 +0900

    commt

--- src/main/java/com/globalin/controller/BoardController.java
+++ src/main/java/com/globalin/controller/BoardController.java
@@ -1,5 +1,85 @@
 package com.globalin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.globain.domain.BoardVO;
+
+@Controller
+@RequestMapping("/board/*")
 public class BoardController {
 
+	@Autowired
+	private BoardService service;
+
+	private static Logger log = LoggerFactory.getLogger(BoardController.class);
+
+	@GetMapping("/list")
+	public void list(Criteria cri, Model model) {
+		log.info("list : " + cri);
+		model.addAttribute("list", service.getList(cri));
+		int total = service.getTotal(cri);
+		log.info("total : " + total);
+		model.addAttribute("pageMaker", new Page(cri, total));
+	}
+
+	@PostMapping("/register")
+	@PreAuthorize("isAuthenticated()")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register : " + board);
+
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+	}
+
+	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") int bno, Criteria cri, Model model) {
+		log.info(" get or modify : " + bno);
+		log.info("cri : " + cri);
+		BoardVO board = service.get(bno);
+		model.addAttribute("cri", cri);
+		model.addAttribute("board", board);
+	}
+
+	@PreAuthorize("principal.username == #board.writer")
+	@PostMapping("/modify")
+	public String modify(BoardVO board, Criteria cri, RedirectAttributes rttr) {
+		log.info("modify : " + board);
+		if (service.modify(board) == true) {
+			// 1회용 속성이 flashAttribute 추가
+			rttr.addFlashAttribute("result", "success");
+		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		return "redirect:/board/list";
+	}
+
+	@PreAuthorize("principal.username == #writer")
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") int bno, Criteria cri, RedirectAttributes rttr, String writer) {
+
+		log.info("remove : " + bno);
+		if (service.remove(bno) == true) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/register")
+	@PreAuthorize("isAuthenticated()")
+	public void register() {
+	}
 }
